e05e0e7f7438e6c4cc7520fa803e98f87d7a421b
ju.rao <ju.rao@kangcenet.com>
2019-10-20 11:58:26 +0800

    搭建基础 http

---
+++ models/in_memo/user.js
@@ -0,0 +1,50 @@
+const users = [];
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.id = User.id++;
+  }
+
+  getName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static insert(firstName, lastName, age) {
+    const u = new User(firstName, lastName, age);
+    User.users.push(u);
+    return u;
+  }
+  static getOneByName(firstName, lastName) {
+    return User.users.find(
+      u => u.firstName === firstName && u.lastName === lastName
+    );
+  }
+  static getOneById(userId) {
+    return User.users.find(u => u.id === userId);
+  }
+
+  static list(query) {
+    return User.users;
+  }
+
+  // 类的静态方法
+  static get ["users"]() {
+    return users;
+  }
+}
+
+User.id = 0;
+
+module.exports = User;
+
+console.log(User.list());
+console.log(User.insert("ke", "ji", 234));
+console.log(User.list());
+
+console.log(User.insert("kes", "jid", 2));
+
+console.log(User.list());
+
+console.log(User.insert("kes", "jid", 2));
---
+++ routes/users.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+// const User = require("../models/in_memo/user");
+const UserService = require("../services/use_service");
+
+/* GET users listing. */
+router.get("/", function(req, res) {
+  const users = UserService.getAllUsers();
+  res.locals.users = users;
+  res.render("users");
+});
+
+router.post("/", function(req, res) {
+  const { firstName, lastName, age } = req.body;
+  const u = UserService.addNewUser(firstName, lastName, age);
+  res.json(u);
+});
+
+router.get("/:userId", function(req, res) {
+  const user = UserService.getUserById(Number(req.params.userId));
+  res.locals.user = user;
+  res.render("user");
+});
+
+router.post("/:userId/subscription", function(req, res, next) {
+  try {
+    const sub = UserService.createSubscription(
+      Number(req.params.userId),
+      req.body.url
+    );
+    res.json(sub);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
