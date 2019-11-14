6957e590b5ec64c4fdf749422d9d22f908d4f70c
ju.rao <ju.rao@kangcenet.com>
2019-11-14 15:39:29 +0800

    密钥

---
+++ cipher/jwt_config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SECRET: "FASSDFSF",
+  expireIn: 60 * 60 * 1000 * 24
+};
---
+++ cipher/password_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SALT: "dddsffgdgerghjyjk",
+  ITERATIONS: 10000,
+  KEY_LENGTH: 512,
+  DIGEST: "sha256"
+};
